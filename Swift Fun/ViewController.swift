3ba64278950e2e31b10c326c434673f33425a480
Johan Lundberg <johanlundberg@johans-macbook-pro.local>
2019-01-12 21:29:51 +0100

    Initial Commit

---
+++ Swift Fun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Fun
+//
+//  Created by Johan Lundberg on 2019-01-12.
+//  Copyright © 2019 Johan Lundberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
